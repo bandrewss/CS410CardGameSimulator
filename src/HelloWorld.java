f98391472e1356620b16802de6445315c17b2bc6
MLadin <MLadin15@winona.edu>
2017-09-24 20:59:15 -0500

    Changes for Card Object
    Suit is now a string to work with the unicode

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,7 +9,8 @@ public class HelloWorld {
 	 * This is a method comment
 	 */
 	public static void main(String[] args) {
-		
+		String heart= "\u2665";
+		System.out.printf(heart);
 		System.out.printf("Hello World\n");
 
 	}
