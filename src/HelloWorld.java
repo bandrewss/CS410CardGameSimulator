a69c4f0c20a918e59d2ad8e5c80877378ddeaad8
Ben Andrews <ben13100@msn.com>
2017-09-24 19:02:50 -0500

    Added code style to helloworld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,15 @@
+// This is a class comment 
+// Always explicit braces 
+// not cuddled elses
 
-public class HelloWorld 
-{
 
-	public static void main(String[] args) 
-	{
+public class HelloWorld {
+	
+	/*
+	 * This is a method comment
+	 */
+	public static void main(String[] args) {
+		
 		System.out.printf("Hello World\n");
 
 	}
