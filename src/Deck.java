785bc3eb078a3166c4a67484a0c76ed6dfc7872b
Waheed Khan <waheedkhan922@gmail.com>
2017-09-26 20:43:42 -0500

    commens + new function
    added some comments with Matt and added a new method for dealing in the deck class

--- src/Deck.java
+++ src/Deck.java
@@ -1,12 +1,16 @@
 
 public class Deck {
 
+// variables for symbols
 	private String heart= "\uu2300";
 	private String spade= "\uu2660";
 	private String dimond= "\uu2666";
 	private String club= "\uu2663";
 	private String suits[]= {heart, spade,dimond,club};
+	
+	//array of 52 cards
 	private Card deckArray[]= new Card[51];
+	
 	
 	Deck(){
 		for(int i=0;i<=suits.length-1;i++) {
@@ -26,8 +30,13 @@ public class Deck {
             	deckArray[i] = deckArray[rand];
             	deckArray[rand] = temp;
         	}//End For loop
+    		
     	}//End Reshuffle loop 
     }//End shuffle
-
+//Deal function to deal the hand to each player
+    public void deal() {
+    	
+    	
+    }
 
 }
