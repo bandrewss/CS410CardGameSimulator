de893d5d2abfc6d06d1c806aa5a4f6fb8c2ad526
Ben Andrews <ben13100@msn.com>
2017-10-07 17:36:52 -0500

    Changed GUI to a cleaner grid layout.

--- src/GUI.java
+++ src/GUI.java
@@ -1,135 +1,66 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 public class GUI extends JFrame {
 	
 	private JTextArea display;
-	private JButton[] cardButtons = new JButton[17];
+	private JButton[] cardButtons;
 	private JScrollPane jScrollPane1;
+	private JFrame frame;
+	private JPanel gridPanel;
+	private GridLayout grid;
 	
-	Client client;
+	private Client client;
 	
 	
 	
 	public GUI(String name, Client c) {
 		client = c;
 		
+		frame = new JFrame();
 		
-		jScrollPane1 = new javax.swing.JScrollPane();
-		display = new javax.swing.JTextArea();
-		display.setEditable(false);
-
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-		jScrollPane1 = new JScrollPane();
-		display = new JTextArea();
-
+		gridPanel = new JPanel();
+		grid = new GridLayout(6, 3);
+		gridPanel.setLayout(grid);
+		
+		cardButtons = new JButton[17];
+		
 		for (int i = 0; i < cardButtons.length; ++i) {
 			cardButtons[i] = new JButton();
 			String cardName =String.format("%d", i);
 			cardButtons[i].setText(cardName);
-		}
-		for (int i = 0; i < cardButtons.length; ++i) {
-			 
+			gridPanel.add(cardButtons[i]);
+			
 			cardButtons[i].addActionListener(new java.awt.event.ActionListener() {
 	            public void actionPerformed(java.awt.event.ActionEvent evt) {
 	            	jButtonActionPerformed(evt);
 	            }
 	        });
 		}
-
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-		display.setColumns(20);
-		display.setRows(8);
+		
+		jScrollPane1 = new JScrollPane();
+		display = new JTextArea();
+		display.setEditable(false);
+		display.setColumns(25);
+		display.setRows(10);
 		jScrollPane1.setViewportView(display);
 
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-		getContentPane().setLayout(layout);
-		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(layout.createSequentialGroup()
-						.addGroup(layout
-								.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
-										.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-										.addGroup(layout.createSequentialGroup().addGap(57, 57, 57).addComponent(
-												jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 277,
-												javax.swing.GroupLayout.PREFERRED_SIZE))
-										.addGroup(layout
-												.createSequentialGroup().addContainerGap().addComponent(cardButtons[0])
-												.addGap(18, 18, 18)
-												.addComponent(cardButtons[1])
-												.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-												.addComponent(cardButtons[2]))
-										.addGroup(layout.createSequentialGroup().addContainerGap().addGroup(layout
-												.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-												.addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout
-														.createSequentialGroup().addComponent(cardButtons[6])
-														.addPreferredGap(
-																javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-														.addComponent(cardButtons[7])
-														.addPreferredGap(
-																javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-																javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-														.addComponent(cardButtons[8]))
-												.addGroup(javax.swing.GroupLayout.Alignment.LEADING,
-														layout.createSequentialGroup()
-																.addComponent(cardButtons[3])
-																.addPreferredGap(
-																		javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-																.addComponent(cardButtons[4]).addGap(18, 18, 18)
-																.addComponent(cardButtons[5]))))
-										.addGroup(javax.swing.GroupLayout.Alignment.TRAILING,
-												layout.createSequentialGroup().addContainerGap().addGroup(layout
-														.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-														.addGroup(layout.createSequentialGroup()
-																.addComponent(cardButtons[12])
-																.addPreferredGap(
-																		javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-																.addComponent(cardButtons[13])
-																.addPreferredGap(
-																		javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-																.addComponent(cardButtons[14]))
-														.addGroup(layout.createSequentialGroup()
-																.addComponent(cardButtons[9])
-																.addPreferredGap(
-																		javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-																.addComponent(cardButtons[10])
-																.addPreferredGap(
-																		javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-																.addComponent(cardButtons[11])))))
-								.addGroup(
-										layout.createSequentialGroup().addGap(62, 62, 62).addComponent(cardButtons[15])
-												.addGap(30, 30, 30).addComponent(cardButtons[16])))
-						.addContainerGap(66, Short.MAX_VALUE)));
-		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
-				.createSequentialGroup().addContainerGap()
-				.addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE,
-						javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-				.addGap(18, 18, 18)
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(cardButtons[0]).addComponent(cardButtons[1]).addComponent(cardButtons[2]))
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(cardButtons[3]).addComponent(cardButtons[4]).addComponent(cardButtons[5]))
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(cardButtons[6]).addComponent(cardButtons[7]).addComponent(cardButtons[8]))
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(cardButtons[9]).addComponent(cardButtons[10]).addComponent(cardButtons[11]))
-				.addGap(18, 18, 18)
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(cardButtons[12]).addComponent(cardButtons[13]).addComponent(cardButtons[14]))
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(cardButtons[15]).addComponent(cardButtons[16]))
-				.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
-
-		pack();
-		setSize(400, 500);
-		setTitle(name);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		setAlwaysOnTop(true);
+		frame.add(jScrollPane1, BorderLayout.NORTH);
+		frame.add(gridPanel, BorderLayout.CENTER);
+		frame.setSize(400, 500);
+		frame.setTitle(name);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setAlwaysOnTop(true);
+	}
+	
+	/*
+	 * Sets the window title.
+	 */
+	public void setTitle(String s) {
+		frame.setTitle(s);
 	}
 	
 	/*
